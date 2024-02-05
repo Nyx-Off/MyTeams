65f2ed52c58d67889b99b36af29f4add3bcc1137
Nyx-Off <contact@bensalemdev.fr>
2024-02-05 09:36:19 +0100

    factorisation du code

--- client.c
+++ client.c
@@ -7,48 +7,34 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define BUFFER_SIZE 1024
+
+// Prototypes de fonctions
+void init_connection(int *sock, struct sockaddr_in *serverAddr, char *serverAddress, int port);
+void send_pseudo(int sock, char *pseudo);
+void handle_user_input(int sock);
+void receive_message(int sock);
+void close_connection(int sock);
+
 int main(int argc, char *argv[]) {
     if (argc != 4) {
         fprintf(stderr, "Usage: %s <Server Address> <Port> <Pseudo>\n", argv[0]);
         return 1;
     }
 
+    int sock;
+    struct sockaddr_in serverAddr;
     char *serverAddress = argv[1];
     int port = atoi(argv[2]);
     char *pseudo = argv[3];
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("socket");
-        return 1;
-    }
-
-    struct sockaddr_in serverAddr;
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    if (inet_pton(AF_INET, serverAddress, &serverAddr.sin_addr) <= 0) {
-        perror("inet_pton");
-        close(sock);
-        return 1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("connect");
-        close(sock);
-        return 1;
-    }
-
-    if (send(sock, pseudo, strlen(pseudo), 0) < 0) {
-        perror("send pseudo");
-        close(sock);
-        return 1;
-    }
+    init_connection(&sock, &serverAddr, serverAddress, port);
+    send_pseudo(sock, pseudo);
 
     printf("Connecté au serveur %s:%d en tant que %s\n", serverAddress, port, pseudo);
 
     fd_set read_fds;
-    int fd_max = sock > STDIN_FILENO ? sock : STDIN_FILENO;
+    int fd_max = sock;
 
     while (1) {
         FD_ZERO(&read_fds);
@@ -61,32 +47,75 @@ int main(int argc, char *argv[]) {
         }
 
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-            char message[1024];
-            if (fgets(message, 1024, stdin) == NULL) break; // Sortie si fgets échoue
-
-            size_t len = strlen(message);
-            if (message[len - 1] == '\n') message[len - 1] = '\0';
-
-            if (strcmp(message, "exit") == 0) break;
-
-            if (send(sock, message, strlen(message), 0) < 0) {
-                perror("send");
-                break;
-            }
+            handle_user_input(sock);
         }
 
         if (FD_ISSET(sock, &read_fds)) {
-            char buffer[1024];
-            memset(buffer, 0, sizeof(buffer));
-            ssize_t len = recv(sock, buffer, sizeof(buffer), 0);
-            if (len <= 0) {
-                printf("Déconnexion du serveur.\n");
-                break;
-            }
-            printf("==> %s\n", buffer); // Affiche déjà le pseudo et le message
+            receive_message(sock);
         }
     }
 
-    close(sock);
+    close_connection(sock);
     return 0;
+}
+
+void init_connection(int *sock, struct sockaddr_in *serverAddr, char *serverAddress, int port) {
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    memset(serverAddr, 0, sizeof(*serverAddr));
+    serverAddr->sin_family = AF_INET;
+    serverAddr->sin_port = htons(port);
+    if (inet_pton(AF_INET, serverAddress, &serverAddr->sin_addr) <= 0) {
+        perror("inet_pton");
+        close(*sock);
+        exit(1);
+    }
+
+    if (connect(*sock, (struct sockaddr *)serverAddr, sizeof(*serverAddr)) < 0) {
+        perror("connect");
+        close(*sock);
+        exit(1);
+    }
+}
+
+void send_pseudo(int sock, char *pseudo) {
+    if (send(sock, pseudo, strlen(pseudo), 0) < 0) {
+        perror("send pseudo");
+        close(sock);
+        exit(1);
+    }
+}
+
+void handle_user_input(int sock) {
+    char message[BUFFER_SIZE];
+    if (fgets(message, BUFFER_SIZE, stdin) == NULL) return; // Sortie si fgets échoue
+
+    size_t len = strlen(message);
+    if (message[len - 1] == '\n') message[len - 1] = '\0';
+
+    if (strcmp(message, "exit") == 0) exit(0);
+
+    if (send(sock, message, strlen(message), 0) < 0) {
+        perror("send");
+        exit(1);
+    }
+}
+
+void receive_message(int sock) {
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    ssize_t len = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (len <= 0) {
+        printf("Déconnexion du serveur.\n");
+        exit(0);
+    }
+    printf("==> %s\n", buffer);
+}
+
+void close_connection(int sock) {
+    close(sock);
 }
--- server.c
+++ server.c
@@ -11,17 +11,17 @@
 #define BUFFER_SIZE 1024
 #define PSEUDO_SIZE 32
 
-char client_pseudos[MAX_CLIENTS][PSEUDO_SIZE];
+// Variables globales pour simplifier
+int client_socks[MAX_CLIENTS] = {0};
+char client_pseudos[MAX_CLIENTS][PSEUDO_SIZE] = {0};
+int total_clients = 0;
 
-void broadcast_message(int sender_sock, int *client_socks, char client_pseudos[MAX_CLIENTS][PSEUDO_SIZE], int total_clients, char *message, char *sender_pseudo) {
-    for (int i = 0; i < total_clients; i++) {
-        if (client_socks[i] != sender_sock) { // Ne pas renvoyer le message au client qui l'a envoyé
-            char full_message[BUFFER_SIZE + PSEUDO_SIZE];
-            snprintf(full_message, sizeof(full_message), "%s : %s", sender_pseudo, message); // Utiliser le pseudo de l'expéditeur
-            send(client_socks[i], full_message, strlen(full_message), 0);
-        }
-    }
-}
+// Prototypes de fonctions
+void init_server(int *server_sock, struct sockaddr_in *server_addr, int port);
+void handle_new_connection(int server_sock, fd_set *master_fds, int *fd_max);
+void handle_client_message(int client_sock, fd_set *master_fds);
+void broadcast_message(int sender_sock, char *message, char *sender_pseudo);
+void close_socket(int sock, fd_set *master_fds);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -30,36 +30,13 @@ int main(int argc, char *argv[]) {
     }
 
     int port = atoi(argv[1]);
-    int server_sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_size;
+    int server_sock;
+    struct sockaddr_in server_addr;
 
     fd_set read_fds, master_fds;
-    int fd_max, client_socks[MAX_CLIENTS], total_clients = 0;
+    int fd_max;
 
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
-        perror("socket");
-        return 1;
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
-
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind");
-        close(server_sock);
-        return 1;
-    }
-
-    if (listen(server_sock, MAX_CLIENTS) < 0) {
-        perror("listen");
-        close(server_sock);
-        return 1;
-    }
-
+    init_server(&server_sock, &server_addr, port);
     FD_ZERO(&master_fds);
     FD_ZERO(&read_fds);
     FD_SET(server_sock, &master_fds);
@@ -67,8 +44,6 @@ int main(int argc, char *argv[]) {
 
     printf("Le serveur démarre sur le port %d.\n", port);
     printf("En attente de messages des clients...\n");
-
-    
 
     while (1) {
         read_fds = master_fds;
@@ -80,48 +55,99 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i <= fd_max; i++) {
             if (FD_ISSET(i, &read_fds)) {
                 if (i == server_sock) {
-                    client_addr_size = sizeof(client_addr);
-                    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
-                    if (client_sock < 0) {
-                        perror("accept");
-                        continue;
-                    }
-
-                    FD_SET(client_sock, &master_fds);
-                    client_socks[total_clients++] = client_sock;
-                    if (client_sock > fd_max) {
-                        fd_max = client_sock;
-                    }
-
-                    printf("Nouveau client connecté : socket %d\n", client_sock);
+                    handle_new_connection(server_sock, &master_fds, &fd_max);
                 } else {
-                    char buffer[BUFFER_SIZE];
-                    memset(buffer, 0, BUFFER_SIZE);
-                    ssize_t len = recv(i, buffer, BUFFER_SIZE, 0);
-
-                    if (len <= 0) {
-                        printf("Client déconnecté : socket %d\n", i);
-                        close(i);
-                        FD_CLR(i, &master_fds);
-                    } else {
-                        buffer[len] = '\0';
-
-                        if (client_pseudos[i][0] == '\0') {
-                            // Attribuer le pseudo au client pour la première fois
-                            strncpy(client_pseudos[i], buffer, PSEUDO_SIZE - 1);
-                            client_pseudos[i][PSEUDO_SIZE - 1] = '\0'; // Assurer la terminaison de la chaîne
-                            printf("Pseudo '%s' attribué au client %d\n", buffer, i);
-                        } else {
-                            // Diffuser le message aux autres clients
-                            printf("Message reçu de %s: %s\n", client_pseudos[i], buffer);
-                            broadcast_message(i, client_socks, client_pseudos, total_clients, buffer, client_pseudos[i]);
-                        }
-                    }
+                    handle_client_message(i, &master_fds);
                 }
             }
         }
     }
 
-
+    // Fermeture du socket serveur
+    close(server_sock);
     return 0;
+}
+
+void init_server(int *server_sock, struct sockaddr_in *server_addr, int port) {
+    *server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (*server_sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    memset(server_addr, 0, sizeof(*server_addr));
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_addr.s_addr = INADDR_ANY;
+    server_addr->sin_port = htons(port);
+
+    if (bind(*server_sock, (struct sockaddr *)server_addr, sizeof(*server_addr)) < 0) {
+        perror("bind");
+        close(*server_sock);
+        exit(1);
+    }
+
+    if (listen(*server_sock, MAX_CLIENTS) < 0) {
+        perror("listen");
+        close(*server_sock);
+        exit(1);
+    }
+}
+
+void handle_new_connection(int server_sock, fd_set *master_fds, int *fd_max) {
+    int client_sock;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+
+    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
+    if (client_sock < 0) {
+        perror("accept");
+        return;
+    }
+
+    FD_SET(client_sock, master_fds);
+    client_socks[total_clients++] = client_sock;
+    if (client_sock > *fd_max) {
+        *fd_max = client_sock;
+    }
+
+    printf("Nouveau client connecté : socket %d\n", client_sock);
+}
+
+void handle_client_message(int client_sock, fd_set *master_fds) {
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    ssize_t len = recv(client_sock, buffer, BUFFER_SIZE, 0);
+
+    if (len <= 0) {
+        printf("Client déconnecté : socket %d\n", client_sock);
+        close_socket(client_sock, master_fds);
+    } else {
+        buffer[len] = '\0';
+        char *sender_pseudo = client_pseudos[client_sock];
+
+        if (sender_pseudo[0] == '\0') {
+            strncpy(sender_pseudo, buffer, PSEUDO_SIZE - 1);
+            sender_pseudo[PSEUDO_SIZE - 1] = '\0';
+            printf("Pseudo '%s' attribué au client %d\n", buffer, client_sock);
+        } else {
+            printf("Message reçu de %s: %s\n", sender_pseudo, buffer);
+            broadcast_message(client_sock, buffer, sender_pseudo);
+        }
+    }
+}
+
+void broadcast_message(int sender_sock, char *message, char *sender_pseudo) {
+    for (int i = 0; i < total_clients; i++) {
+        if (client_socks[i] != sender_sock) {
+            char full_message[BUFFER_SIZE + PSEUDO_SIZE];
+            snprintf(full_message, sizeof(full_message), "%s : %s", sender_pseudo, message);
+            send(client_socks[i], full_message, strlen(full_message), 0);
+        }
+    }
+}
+
+void close_socket(int sock, fd_set *master_fds) {
+    close(sock);
+    FD_CLR(sock, master_fds);
+    // Ici, on pourrait également gérer la suppression du socket de la liste des clients
 }
