f48aa54470f8c117b90e85c3d90a6d4870f8744e
Nyx-Off <contact@bensalemdev.fr>
2024-02-15 11:15:42 +0100

    [BUG] commande /info, /help, /who, /nickname, clear des includes  DONE

--- client.c
+++ client.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
--- server.c
+++ server.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <ncurses.h>
 #include <ctype.h>
 #include <time.h>
-#include <poll.h>
 #include <sqlite3.h>
 
 
